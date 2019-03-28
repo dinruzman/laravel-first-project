4bcdde075b1ff51648afb13db492be21c4313aac
dinruzman <dinruzman@gmail.com>
2019-03-28 14:38:42 +0800

    update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,54 +1,39 @@
-@extends('layouts.app')
-
+@extends ('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard
-                <a href ="{{url('tasks/create')}}" class="btn btn-success float-right" >Create new tasks </a>
-                </div>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 2px;
+        text-align: left;
+    }
+</style>
 
-                    <table class="table table-striped">
-                    <tr>
-                        <th>ID</th>
-                        <th>Tasks</th>
-                        <th>pic</th>
-                        <th>Email</th>
-                        <th>Phone No</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    @foreach ($tasks as $task)
-                    <tr>
-                        <td>{{$task->id}}</td>
-                        <td>{{$task->name}}</td>
-                        <td>{{$task->pic}}</td>
-                        <td>{{$task->email}}</td>
-                        <td>{{$task->phone_no}}</td>
-                        <td>
-                        <form action="{{url('tasks/'.$task->id)}}" method="POST">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="_method" value="DELETE">
-                            <button class="btn btn-danger float-right" >Delete</button>
-                        </form>
-                        </td>
-                        <td>
-                            <a href="{{url('tasks/'.$task->id.'/edit')}}" class="btn btn-warning float-right">Edit</a>
-                        </td>
-                        </tr>
-                    @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+</head>
+<body>
+<table style="width:50%">
+<tr>
+    <th>Tasks</th>
+    <th>pic</th>
+    <th>Email</th>
+    <th>Phone No</th>
+</tr>
+@foreach ($tasks as $task)
+<tr>
+    <td>{{$task -> name}}</td>
+    <td>{{$task -> pic}}</td>
+    <td>{{$task -> email}}</td>
+    <td>{{$task -> phone_no}}</td>
+</tr>
+@endforeach
+</table>
 @endsection
